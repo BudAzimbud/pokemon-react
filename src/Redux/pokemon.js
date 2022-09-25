f038d158a22506328f17c23a577f064fac053e56
azim <budazimbud@gmail.com>
2022-09-25 17:43:36 +0700

    search to lowercase

--- src/Redux/pokemon.js
+++ src/Redux/pokemon.js
@@ -13,7 +13,7 @@ export const getAllPokemon = createAsyncThunk("pokemons/list",async({limit})=>{
 })
 
 export const findPokemon = createAsyncThunk("pokemons/find",async({name})=>{
-    const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
     console.log(pokemon)
     return [pokemon.data]      
 })
